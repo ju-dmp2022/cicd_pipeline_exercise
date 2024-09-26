fe089ab040f4d19480b74f665b0b1a5f0738fe56
Lamo <lamochi02@gmail.com>
2024-09-26 15:43:39 +0200

    try 2 without chromedriver tests opening

--- tests/web/web_test.py
+++ tests/web/web_test.py
@@ -1,17 +1,17 @@
-import pytest
-from tests.web.test_base import WebBase
-from tests.web.pages.login_page import LoginPage
+# import pytest
+# from tests.web.test_base import WebBase
+# from tests.web.pages.login_page import LoginPage
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# import time
 
-class TestWeb(WebBase):
+# class TestWeb(WebBase):
 
-    def test_login(self):
-        pass
+#     def test_login(self):
+#         pass
     
 
